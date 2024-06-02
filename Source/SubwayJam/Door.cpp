5eb8b3ae73b3384c1d3980ebcfb190b55c2a5383
Wesley Thompson <a.wes.thompson@gmail.com>
2024-06-02 18:30:56 -0400

    Add Door class

---
+++ Source/SubwayJam/Door.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Door.h"
+
+// Sets default values for this component's properties
+UDoor::UDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CurrentPosition = GetOwner()->GetActorLocation();
+	UE_LOG(LogTemp, Display, TEXT("After: %s"), *CurrentPosition.ToCompactString());
+}
+
+
+// Called every frame
+void UDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	
+}
+
